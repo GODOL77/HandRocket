f932c6d580ef90a339e70a1e065ad8612704cf9e
GODOL <sihyun9346@mokwon.ac.kr>
2025-12-11 12:27:22 +0900

    Reapply "modify Unity camera, add Rocket Gravity, editing RocketManager"
    This reverts commit 29274aa999f741434b3988e761200acf7639cca6.

--- HandTracking/Assets/Scripts/RocketManager.cs
+++ HandTracking/Assets/Scripts/RocketManager.cs
@@ -6,8 +6,9 @@ public class RocketManager : MonoBehaviour
 
     [Header("Game Settings")]
     //public float countdownTime = 20f;
-
+    public Rigidbody rocketRb;
     public GameObject Rocket;
+    public float powerMultiplier = 100f;
 
     [Header("Rocket State")]
     public float fuel = 0f;                 // 충전된 연료의 양
@@ -15,7 +16,7 @@ public class RocketManager : MonoBehaviour
     public bool rocketLaunched = false;     // 로켓발사확인함수
     private bool countdownFinished = false; // 20초 쿨다운 끝나면 발사
 
-    int rocketPower = 1;
+    public int rocketPower = 0;
 
     public float curTime = 0f;
 
@@ -30,23 +31,22 @@ public class RocketManager : MonoBehaviour
     void Update()
     {
         
-        if (!countdownFinished)
+        if (!countdownFinished) // 카운트다운이 끝날 때까지 작동
         {
             CheckAssembly();
             CheckFuel();
-        }
             LaunchTimer();
+        }
 
         if (countdownFinished && !rocketLaunched)   // 카운트다운 끝 & 로켓 발사 안되었을 시(완성되었는지 확인하는 if문 필요함)
         {
             CheckRocketPower();
         }
 
-        if (rocketLaunched == true)
-        {
-            LaunchRocket();
-        }
-
+        // if (rocketLaunched == true)
+        // {
+        //     LaunchRocket();
+        // }
     }
 
     private void LaunchTimer()   // 코루틴으로 초세는 함수
@@ -82,21 +82,21 @@ public class RocketManager : MonoBehaviour
         
         if (rocketParts == 3 && fuel > 0f) 
         {
-            if (fuel <= 10)
-            {
-                rocketPower = 1;
-            }
-            else if (fuel <= 20)
-            {
-                rocketPower = 2;
-            }
-            else
-            {
-                rocketPower = 3;
-            }
-            
-            rocketLaunched = true;
+            // if (fuel <= 10)
+            // {
+            //     rocketPower = 1;
+            // }
+            // else if (fuel <= 20)
+            // {
+            //     rocketPower = 2;
+            // }
+            // else
+            // {
+            //     rocketPower = 3;
+            // }
+            rocketPower = (int)fuel;
             Debug.Log("✅ 제한시간 내 발사 준비 완료! 최종 파워: " + rocketPower);
+            LaunchRocket();
         }
         else
         {
@@ -106,6 +106,10 @@ public class RocketManager : MonoBehaviour
 
     void LaunchRocket()
     {
-        Rocket.transform.position += new Vector3(0, fuel, 0);
+        rocketLaunched = true;
+
+        rocketRb.isKinematic = false;   // 물리 힘 받도록 설정
+        rocketRb.AddForce(Vector3.up * rocketPower * powerMultiplier, ForceMode.Impulse);
+        // Rocket.transform.position += new Vector3(0, rocketPower * 0.1f, 0);
     }
 }
